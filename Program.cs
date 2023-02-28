b69891638333db4a9b5afc3f42556ae6a36ab357
Wild Mosquito <nailnh@code.edu.az>
2023-02-28 06:06:26 +0400

    task00

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿#region task1
+
+
+//int num = 0;
+
+//sum(1, 2, 3, 4);
+
+//void sum(params int[] nums)
+//{
+//    for (int i = 0; i < nums.Length; i++)
+//    {
+//        if (nums[i] == num)
+//        {
+//            Console.WriteLine("there is");
+
+//        }
+//    } 
+//}
+
+
+
+#endregion
+
+
+#region task2
+
+int sum(params int[] values)
+{
+    int largest = values[0];
+    for (int i = 0; i < values.Length; i++)
+    {
+        if (values[i] > largest)
+            largest = values[i];
+    }
+    return largest;
+}
+Console.WriteLine(sum(1,3,5,2,6,8,7));
+
+#endregion
